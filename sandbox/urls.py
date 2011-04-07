bc7c5439fe753887ed5fb2c86a08aa70cfd192c8
haineault@gmail.com <haineault@gmail.com@3956e0aa-3a16-c8c5-73a3-1ff85494d0a9>
2011-04-07 01:39:25 +0000

    added debug toolbar

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -8,15 +8,13 @@ import mobileadmin
 mobileadmin.autoregister()
 
 urlpatterns = patterns('',
-    (r'^admin/',     include(admin.site.urls)),
-    (r'^ma/',        include(mobileadmin.sites.site.urls)),
-    (r'^grappelli/', include('contrib.grappelli.urls')),
-
+    (r'^admin/',        include(admin.site.urls)),
+    (r'^ma/',           include(mobileadmin.sites.site.urls)),
+    (r'^grappelli/',    include('contrib.grappelli.urls')),
 )
 
 if settings.DEBUG:
     urlpatterns += patterns('',
-        (r'^%s(?P<path>.*)$' % settings.STATIC_URL[1:],  'django.contrib.staticfiles.views.serve'),
-        (r'^%s(.*)$' % settings.MEDIA_URL[1:],           'django.views.static.serve', {'document_root': 'media'}),
-        (r'^%s(.*)$' % settings.ADMIN_MEDIA_PREFIX[1:],  'django.views.static.serve', {'document_root': '../contrib/grappelli/media/'}),
+        (r'^%s(.*)$' % settings.MEDIA_URL[1:], 
+            'django.views.static.serve', {'document_root': 'media'}),
     )

cd03eafb33e24c91fe35a47312c4de21cb1065d9
haineault@gmail.com <haineault@gmail.com@3956e0aa-3a16-c8c5-73a3-1ff85494d0a9>
2011-04-22 03:29:09 +0000

    removed django-mobileadmin dependency in standalone branch

---
+++ branches/standalone/sites.py
@@ -0,0 +1,29 @@
+from django.contrib.admin.sites import AdminSite, site
+from django.views.decorators.cache import never_cache
+from django.contrib.auth.views import password_change, password_change_done, logout
+
+
+class MobileAdminSite(AdminSite):
+#   login_form = None
+    index_template = 'jqmobile/index.html' 
+    app_index_template = 'jqmobile/app_index.html'
+    login_template = 'jqmobile/login.html'
+    logout_template = 'jqmobile/logout.html'
+    password_change_template = 'jqmobile/registration/password_change_form.html'
+    password_change_done_template = 'jqmobile/registration/password_change_done.html'
+
+    
+
+#   def get_urls(self):
+#       from django.conf.urls.defaults import patterns, url
+
+#       urls = super(MobileAdminSite, self).get_urls()
+#       urls += patterns('',
+#           url(r'^my_view/$', self.admin_view(some_view))
+#       )
+#       return urls
+#   def my_view(self, request):
+#       # custom view which should return an HttpResponse
+#       pass
+
+jqmobile = MobileAdminSite(name='jqmobile')

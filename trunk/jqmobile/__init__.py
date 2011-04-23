f933a10925cd59e1132c420f3a41b6a45182f29e
haineault@gmail.com <haineault@gmail.com@3956e0aa-3a16-c8c5-73a3-1ff85494d0a9>
2011-04-23 12:17:29 +0000

    refactored admin registration to solve conflict between vanilla admin and mobile admin. Changed theme to fit better with grappelli

--- trunk/jqmobile/__init__.py
+++ trunk/jqmobile/__init__.py
@@ -2,39 +2,38 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.admin import site as main_site
 from jqmobile.sites import jqmobile as site
 
-TEMPLATE_MAPPING = {
-#   'display_login_form': ('login_template', 'jqmobile/login.html'),
-    'render_change_form': ('change_form_template', 'jqmobile/change_form.html'),
-    'changelist_view': ('change_list_template', 'jqmobile/change_list.html'),
-    'delete_view': ('delete_confirmation_template', 'jqmobile/delete_confirmation.html'),
-}
 
-### From http://www2.lib.uchicago.edu/keith/courses/python/class/5/#attrref
 def classlookup(C, name):
-	if C.__dict__.has_key(name):
-		return (1, C.__dict__[name])
-	else:
-		for b in C.__bases__:
+    """ 
+    From http://www2.lib.uchicago.edu/keith/courses/python/class/5/#attrref 
+    """
+    if C.__dict__.has_key(name):
+        return (1, C.__dict__[name])
+    else:
+        for b in C.__bases__:
 		    success, value = classlookup(b, name)
 		    if success:
 			    return (1, value)
 		    else:
 			    pass
-		else:
+        else:
 		    return (0, None)
+
 
 def autodiscover():
     """
     Auto-register all ModelAdmin instances of the default AdminSite
     """
-    
+    import copy
+
     for model, modeladmin in main_site._registry.iteritems():
-        admin_class = modeladmin.__class__
-        setattr(admin_class, 'change_form_template', 'jqmobile/change_form.html')
-        setattr(admin_class, 'change_list_template', 'jqmobile/change_list.html')
-        setattr(admin_class, 'object_history_template', 'jqmobile/object_history.html')
-        setattr(admin_class, 'delete_confirmation_template', 'jqmobile/delete_confirmation.html')
-        setattr(admin_class, 'delete_selected_confirmation_template', 'jqmobile/delete_selected_confirmation.html')
+        admin_class = type(modeladmin.__class__.__name__, (modeladmin.__class__,), {
+            'change_form_template': 'jqmobile/change_form.html',
+            'change_list_template': 'jqmobile/change_list.html',
+            'object_history_template': 'jqmobile/object_history.html',
+            'delete_confirmation_template': 'jqmobile/delete_confirmation.html',
+            'delete_selected_confirmation_template': 'jqmobile/delete_selected_confirmation.html',
+        })
                 
         if admin_class == UserAdmin:
             setattr(admin_class, 'add_form_template', 'jqmobile/auth/user/add_form.html')

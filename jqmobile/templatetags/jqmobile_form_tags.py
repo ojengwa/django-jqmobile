c3e6c6888d3818daf2ebefeb4f1b6910479eead7
haineault@gmail.com <haineault@gmail.com@3956e0aa-3a16-c8c5-73a3-1ff85494d0a9>
2011-04-24 04:45:10 +0000

    bug fixes, cleanup, made search work

--- jqmobile/templatetags/jqmobile_form_tags.py
+++
@@ -1,6 +0,0 @@
-from django import template
-from django.contrib.admin.templatetags.admin_modify import prepopulated_fields_js as pfj
-
-register = template.Library()
-
-prepopulated_fields_js = register.inclusion_tag('jqmobile/prepopulated_fields_js.html', takes_context=True)(pfj)

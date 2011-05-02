52fbac41eac1b259eb5622e4fd14b7df5fd0b7e8
haineault@gmail.com <haineault@gmail.com@3956e0aa-3a16-c8c5-73a3-1ff85494d0a9>
2011-05-02 14:53:41 +0000

    added test file and model file

---
+++ trunk/jqmobile/tests.py
@@ -0,0 +1,41 @@
+# http://docs.djangoproject.com/en/dev/topics/testing/
+from django.utils import unittest
+from django.test.client import Client
+
+class JqmobileTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_login(self):
+        # Issue a GET request.
+        response = self.client.get('/ma/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+#       # Check that the rendered context contains 5 customers.
+#       self.assertEqual(len(response.context['customers']), 5)
+
+
+
+
+
+# simulate normal request
+# c = Client()
+# response = c.post('/login/', {'username': 'john', 'password': 'smith'})
+# response.status_code
+
+
+# simulate ajax request
+# c = Client()
+# c.get('/customers/details/', {'name': 'fred', 'age': 7},
+#      HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+
+
+# simulate post request
+
+# c = Client()
+# f = open('wishlist.doc')
+# c.post('/customers/wishes/', {'name': 'fred', 'attachment': f})
+# f.close()

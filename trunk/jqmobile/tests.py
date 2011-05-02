8d2f2efd728d8055eea89b6698beebf0f11e4254
haineault@gmail.com <haineault@gmail.com@3956e0aa-3a16-c8c5-73a3-1ff85494d0a9>
2011-05-02 17:07:27 +0000

    modified tests

--- trunk/jqmobile/tests.py
+++ trunk/jqmobile/tests.py
@@ -1,17 +1,19 @@
 # http://docs.djangoproject.com/en/1.3/topics/testing/
 from django.utils import unittest
+from django.core.urlresolvers import reverse
 from django.test.client import Client
 
-class JqmobileTest(unittest.TestCase):
+class TestJqmobile(unittest.TestCase):
     def setUp(self):
-        # Every test needs a client.
         self.client = Client()
 
-    def test_login(self):
-        # Issue a GET request.
-        response = self.client.get('/ma/')
-
-        # Check that the response is 200 OK.
+    def test_urls_anonymously(self):
+        # Admin URLs
+        # http://docs.djangoproject.com/en/dev/ref/contrib/admin/#reversing-admin-urls
+        response = self.client.get(reverse('jqmobile:index'))
+        self.assertEqual(response.status_code, 200)
+        
+        response = self.client.get(reverse('jqmobile:logout'))
         self.assertEqual(response.status_code, 200)
 
 #       # Check that the rendered context contains 5 customers.
